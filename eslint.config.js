a893c395a841c0c2da23599b05d18bef9f868573
NewDev <65420147@kmitl.ac.th>
2026-01-23 01:26:11 +0700

    Add eslint and change version type node

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+const eslint = require("@eslint/js");
+const { defineConfig } = require("eslint/config");
+const tseslint = require("typescript-eslint");
+
+module.exports = defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    ignores: ["eslint.config.js"],
+  },
+);
