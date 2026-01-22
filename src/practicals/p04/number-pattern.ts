e7769754b3064a1186b1d6584e74b8f296ce8fa6
NewDev <65420147@kmitl.ac.th>
2026-01-23 01:22:02 +0700

    Test 1 fix tsconfig

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,14 @@
 // Write your code below
+const g: number = Number(process.argv[2]);
+if (isNaN(g) || g <= 0) {
+}
+for (let i = 0; g > i; i++) {
+  let line = "";
+
+  for (let j = 0; i <= j; j++) {
+    line += "*";
+  }
+
+  console.log(line);
+}
+export {};
